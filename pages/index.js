a21838d6e37a85f6e496bf83ae5278d3455c50d9
Yuichi Yogo <yuichkun@gmail.com>
2021-01-01 19:28:32 +0900

    toggle between PC and SP according to media size

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
 import PC from '../components/PC'
+import SP from '../components/SP'
+import {useMediaLayout} from 'use-media';
 
 export default function Home() {
+  const isWide = useMediaLayout({minWidth: '770px'});
   return (
     <>
       <Head>
         <title>YOGO</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PC />
+      {isWide ? <PC /> : <SP />}
     </>
   )
 }
