f08448a84d62b99ec94a25f777ba6a27aa61f4af
Yuichi Yogo <yuichkun@gmail.com>
2021-01-02 03:20:50 +0900

    add SP title

--- components/SP.jsx
+++ components/SP.jsx
@@ -1,11 +1,22 @@
 import styles from './SP.module.css'
 
+const Background = () => (
+  <div className={styles.bg_container}>
+    <img className={styles.bg_image} src="/sp-bg-image.jpg"></img>
+  </div>
+);
+
 export default function PC() {
   return (
     <div className={styles.container}>
+      <Background />
       <main className={styles.main}>
-        <img className={styles.bg_image} src="/sp-bg-image.jpg"></img>
+        <h1 className={styles.title}>YOGO</h1>
+        {/* <Description />
+        <hr className={styles.separator} />
+        <VideoArea /> */}
       </main>
+      {/* <Footer /> */}
     </div>
   )
 }
