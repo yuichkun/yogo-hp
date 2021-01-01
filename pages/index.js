d868e2efa0c7eae1576b87a1adac9aafa4b0388f
Yuichi Yogo <yuichkun@gmail.com>
2021-01-02 03:44:59 +0900

    responsively preload images

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,19 @@ import PC from '../components/PC'
 import SP from '../components/SP'
 import {useMediaLayout} from 'use-media';
 
+const MEDIA_BREAK_POINT = 850;
+
 export default function Home() {
-  const isWide = useMediaLayout({minWidth: '850px'});
+  const isWide = useMediaLayout({minWidth: `${MEDIA_BREAK_POINT}px`});
   return (
     <>
       <Head>
         <title>OGO</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preload" href="/bg-image-1.png" as="image" />
-        <link rel="preload" href="/bg-image-2.gif" as="image" />
-        <link rel="preload" href="/bg-image-3.png" as="image" />
-        <link rel="preload" href="/sp-bg-image.jpg" as="image" />
+        <link rel="preload" href="/bg-image-1.png" as="image" media={`(min-width: ${MEDIA_BREAK_POINT}px)`} />
+        <link rel="preload" href="/bg-image-2.gif" as="image" media={`(min-width: ${MEDIA_BREAK_POINT}px)`} />
+        <link rel="preload" href="/bg-image-3.png" as="image" media={`(min-width: ${MEDIA_BREAK_POINT}px)`} />
+        <link rel="preload" href="/sp-bg-image.jpg" as="image" media={`(max-width: ${MEDIA_BREAK_POINT}px)`} />
       </Head>
       {isWide ? <PC /> : <SP />}
     </>
