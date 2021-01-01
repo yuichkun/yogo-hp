c536b3b04474935ba3725135f1e6d417960fd6e2
Yuichi Yogo <yuichkun@gmail.com>
2021-01-02 00:05:17 +0900

    refactor components

--- components/PC.jsx
+++ components/PC.jsx
@@ -1,32 +1,48 @@
 import styles from "./PC.module.css";
 
+const Background = () => (
+  <div className={styles.bg_container}>
+    <img className={styles.bg_image} src="/bg-image-1.png"></img>
+    <img className={styles.bg_image} src="/bg-image-2.gif"></img>
+    <img className={styles.bg_image} src="/bg-image-3.png"></img>
+  </div>
+);
+
+const Description = () => (
+  <p className={styles.description}>
+    音楽家。
+    <br />
+    東京藝術大学作曲科卒業。 <br />
+    茨木のり子。
+  </p>
+);
+
+const VideoArea = () => (
+  <div className={styles.video_area}>
+    <video
+      className={styles.quantization}
+      src="https://s3-ap-northeast-1.amazonaws.com/yogo.style/quantization-excerpt.mp4"
+      controls
+      controlsList="nodownload"
+    />
+    <h3 className={styles.video_caption}>Quantization (excerpt)</h3>
+    <img src="play-button.svg" alt="play-button" />
+  </div>
+);
+
+const Footer = () => <footer className={styles.footer}></footer>;
+
 export default function PC() {
   return (
     <div className={styles.container}>
-      <div className={styles.bg_container}>
-        <img className={styles.bg_image} src="/bg-image-1.png"></img>
-        <img className={styles.bg_image} src="/bg-image-2.gif"></img>
-        <img className={styles.bg_image} src="/bg-image-3.png"></img>
-      </div>
+      <Background />
       <main className={styles.main}>
         <h1 className={styles.title}>YOGO</h1>
-        <p className={styles.description}>
-          音楽家。
-          <br />
-          東京藝術大学作曲科卒業。 <br />
-          茨木のり子。
-        </p>
+        <Description />
         <hr />
-        <video
-          className={styles.quantization}
-          src="https://s3-ap-northeast-1.amazonaws.com/yogo.style/quantization-excerpt.mp4"
-          controls
-          controlsList="nodownload"
-        />
-        <h3 className={styles.video_caption}>Quantization (excerpt)</h3>
-        <img src="play-button.svg" alt="play-button"/>
+        <VideoArea />
       </main>
-      <footer className={styles.footer}></footer>
+      <Footer />
     </div>
   );
 }
