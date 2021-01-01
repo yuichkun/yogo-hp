d9949461cdd45e6cce7f8df41867c39286aa9ff7
Yuichi Yogo <yuichkun@gmail.com>
2021-01-02 03:23:10 +0900

    add SP description

--- components/SP.jsx
+++ components/SP.jsx
@@ -1,9 +1,18 @@
-import styles from './SP.module.css'
+import styles from "./SP.module.css";
 
 const Background = () => (
   <div className={styles.bg_container}>
     <img className={styles.bg_image} src="/sp-bg-image.jpg"></img>
   </div>
+);
+
+const Description = () => (
+  <p className={styles.description}>
+    音楽家。
+    <br />
+    東京藝術大学作曲科卒業。 <br />
+    茨木のり子。
+  </p>
 );
 
 export default function PC() {
@@ -12,11 +21,11 @@ export default function PC() {
       <Background />
       <main className={styles.main}>
         <h1 className={styles.title}>YOGO</h1>
-        {/* <Description />
-        <hr className={styles.separator} />
+        <Description />
+        {/* <hr className={styles.separator} />
         <VideoArea /> */}
       </main>
       {/* <Footer /> */}
     </div>
-  )
+  );
 }
