faf4b45a7428fd36aa75c30fbfbe884bcb791cf6
Yuichi Yogo <yuichkun@gmail.com>
2021-01-01 20:22:20 +0900

    apply rough style to PC

--- components/PC.jsx
+++ components/PC.jsx
@@ -1,4 +1,4 @@
-import styles from './PC.module.css'
+import styles from "./PC.module.css";
 
 export default function PC() {
   return (
@@ -9,18 +9,23 @@ export default function PC() {
         <img className={styles.bg_image} src="/bg-image-3.png"></img>
       </div>
       <main className={styles.main}>
-        <h1>YOGO</h1>
+        <h1 className={styles.title}>YOGO</h1>
         <p className={styles.description}>
-          音楽家。<br />
+          音楽家。
+          <br />
           東京藝術大学作曲科卒業。 <br />
           茨木のり子。
         </p>
-        <hr/>
-        <video src="https://s3-ap-northeast-1.amazonaws.com/yogo.style/quantization-excerpt.mp4" controls controlsList="nodownload"></video>
+        <hr />
+        <video
+          className={styles.quantization}
+          src="https://s3-ap-northeast-1.amazonaws.com/yogo.style/quantization-excerpt.mp4"
+          controls
+          controlsList="nodownload"
+        ></video>
         <h3>Quantization (excerpt)</h3>
       </main>
-      <footer className={styles.footer}>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
