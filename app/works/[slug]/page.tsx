38e1958381eddf0ad9e2375019e603755f53b292
yuichkun <yuichkun@gmail.com>
2024-03-09 05:12:40 +0900

    enable rendering iframe

--- app/works/[slug]/page.tsx
+++ app/works/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import { getWorkBySlug } from "../../lib/api";
+import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export default async function Page({
   params,
@@ -15,6 +17,31 @@ export default async function Page({
       <h1>
         {title} ({year})
       </h1>
+      {documentToReactComponents(description, {
+        renderNode: {
+          [INLINES.HYPERLINK]: (node, children) => {
+            if (node.data.uri.includes("<iframe")) {
+              return (
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: node.data.uri,
+                  }}
+                />
+              );
+            }
+            return (
+              <a
+                href={node.data.uri}
+                target="__blank"
+                className="text-blue-500"
+              >
+                {children}
+              </a>
+            );
+          },
+        },
+        preserveWhitespace: true,
+      })}
     </main>
   );
 }
