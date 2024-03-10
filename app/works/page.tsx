8d58de9494fa5442eb0a7df3f5e786c600b05125
yuichkun <yuichkun@gmail.com>
2024-03-11 01:35:29 +0900

    change layout of works list

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -4,29 +4,31 @@ import { getWorks } from "../lib/api";
 
 export default async function Works() {
   const works = await getWorks();
+  const IMAGE_SCALE = 0.5;
   return (
     <main className="">
-      <h1>Works</h1>
-      <ul>
+      <ul className="flex flex-wrap gap-8 justify-center">
         {works.items.map((work) => {
           const {
             fields: { slug, title, thumbnail, year },
           } = work;
           return (
-            <li key={work.sys.id} className="">
+            <li
+              key={work.sys.id}
+              className="basis-[640px] h-[360px] p-4 overflow-hidden"
+            >
               <Link className="text-2xl font-bold" href={`works/${slug}`}>
-                <span>
+                <div className="mb-4">
                   {title} ({year})
-                </span>
+                </div>
                 {thumbnail?.fields?.file ? (
-                  <div className="w-96">
+                  <div className="w-full h-full">
                     <Image
                       src={`https:${thumbnail.fields.file.url}`}
                       alt={title}
-                      width={thumbnail.fields.file.details.image?.width ?? 100}
-                      height={
-                        thumbnail.fields.file.details.image?.height ?? 100
-                      }
+                      width={1920 * IMAGE_SCALE}
+                      height={1080 * IMAGE_SCALE}
+                      className="h-full object-cover"
                     />
                   </div>
                 ) : (
