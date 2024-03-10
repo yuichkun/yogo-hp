1793663a772944d8be31244c912fcc03102606e3
yuichkun <yuichkun@gmail.com>
2024-03-11 03:57:12 +0900

    Add contact page with social media links and contact information

---
+++ app/contact/page.tsx
@@ -0,0 +1,62 @@
+export default function Contact() {
+  return (
+    <main>
+      <div>
+        <div className="mb-2">Social Media</div>
+        <ul>
+          <li>
+            X(日本語):{" "}
+            <a
+              href="https://twitter.com/yogo_yuichi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @yogo_yuichi
+            </a>
+          </li>
+          <li>
+            X(English):{" "}
+            <a
+              href="https://twitter.com/yuichi_yogo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @yuichi_yogo
+            </a>
+          </li>
+          <li>
+            Instagram:{" "}
+            <a
+              href="https://www.instagram.com/yuichi_yogo/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @yuichi_yogo
+            </a>
+          </li>
+          <li>
+            GitHub:{" "}
+            <a
+              href="https://github.com/yuichkun"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @yuichkun
+            </a>
+          </li>
+        </ul>
+      </div>
+      <p className="mt-8">
+        お仕事関係のご相談/お問い合わせは、
+        <a
+          href="https://yogo-management-office.com/contact"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          会社HP
+        </a>
+        よりお願いします。
+      </p>
+    </main>
+  );
+}
