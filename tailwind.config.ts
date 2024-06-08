e7bbbdfd9a228b1c9d7751c4682920c9020fac71
yuichkun <yuichkun@gmail.com>
2024-06-08 12:08:00 +0900

    change theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,12 @@
 import type { Config } from "tailwindcss";
 
+const ELECTRIC_VIOLET = "#864AF9";
+const SUN_GLOW = "#F8E559";
+const SNOW_WHITE = "#ffffff";
+const ULTRA_MARINE_BLUE = "#383486";
+const HOT_PINK = "#f5426f";
+const NIGHT_SHADOW = "#261241";
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,7 +15,17 @@ const config: Config = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["acid"],
+    themes: [
+      {
+        mytheme: {
+          primary: SUN_GLOW,
+          secondary: "#f6d860",
+          accent: SUN_GLOW,
+          neutral: SNOW_WHITE,
+          "base-100": ULTRA_MARINE_BLUE,
+        },
+      },
+    ],
   },
 };
 export default config;
