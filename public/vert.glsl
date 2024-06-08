75acce42d97c3139cb7205423f30c551458150ce
yuichkun <yuichkun@gmail.com>
2024-06-08 17:37:01 +0900

    WIP bg shader

--- public/vert.glsl
+++ public/vert.glsl
@@ -1,8 +1,15 @@
-varying vec2 vUv;
+uniform float u_time;
+
+varying float vZ;
 
 void main() {
-  vUv = uv;
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  
+  modelPosition.y += sin(modelPosition.x * 5.0 + u_time * 3.0) * 0.1;
+  modelPosition.y += sin(modelPosition.z * 6.0 + u_time * 2.0) * 0.1;
+  
+  vZ = modelPosition.y;
+
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
