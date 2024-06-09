17702770a5a68ecc9400ffcaf3af78e9810f17d7
yuichkun <yuichkun@gmail.com>
2024-06-09 22:42:30 +0900

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,10 @@
 function NotFoundPage() {
-  return <h1>Page not found</h1>;
+  return (
+    <main className="text-center font-bold text-4xl mt-32 mix-blend-color-burn">
+      <h1 className="">404</h1>
+      <div>Not Found</div>
+    </main>
+  );
 }
 
 export default NotFoundPage;
