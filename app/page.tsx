955170a2977ec68c2bb4fb72030e86c512207dfe
yuichkun <yuichkun@gmail.com>
2024-06-09 20:15:33 +0900

    add news to top page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
-import Image from "next/image";
-import { Background } from "./components/Background";
+// TODO: replace this with CMS
+const news = [
+  {
+    id: 1,
+    date: "2024/06/09",
+    title: "HPを更新しました",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
+    <main className="p-8 font-mono max-w-96 mx-auto">
+      <div className="flex justify-center">
+        <span className="bg-black mb-8">What&apos;s New?</span>
+      </div>
+
+      <ul>
+        {news.map((n) => (
+          <li key={n.id}>
+            <div
+              className="grid grid-cols-2 text-sm bg-gray-200 text-black mix-blend-overlay"
+              style={{
+                gridTemplateColumns: "120px auto",
+              }}
+            >
+              <div className="">{n.date}</div>
+              <div className="text-sm underline">{n.title}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
