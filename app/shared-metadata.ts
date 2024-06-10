4d7e3eac2fed1a0afc7aefd456c356b3c5abbab0
yuichkun <yuichkun@gmail.com>
2024-06-10 21:37:25 +0900

    add more fields to shared metadata

--- app/shared-metadata.ts
+++ app/shared-metadata.ts
@@ -6,4 +6,24 @@ export const SHARED_METADATA: Metadata = {
     default: "YOGO HP",
   },
   description: "The Official Website of Yuichi Yogo",
+  generator: "Next.js",
+  applicationName: "YOGO HP",
+  keywords: [
+    "Yuichi Yogo",
+    "Yogo Yuichi",
+    "Yogo Management Office",
+    "YOGO",
+    "余湖",
+    "余湖雄一",
+    "よご",
+    "よごゆういち",
+  ],
+  authors: [
+    {
+      name: "Yuichi Yogo",
+      url: "https://yogo.style",
+    },
+  ],
+  creator: "Yuichi Yogo",
+  publisher: "Yogo Management Office, LLC",
 };
