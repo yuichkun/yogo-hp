b197837679bd755bf66c84d59681ece23b8f2851
yuichkun <yuichkun@gmail.com>
2024-06-10 19:18:43 +0900

    chore: Update work page layout and styles

--- app/works/[slug]/page.tsx
+++ app/works/[slug]/page.tsx
@@ -15,58 +15,59 @@ export default async function Page({
   } = await getWorkBySlug(params.slug);
 
   return (
-    <main className="mx-auto p-8 max-w-[960px] flex flex-col items-center">
-      <h1 className="text-2xl mb-8">
+    <main className="mx-auto p-8 max-w-[960px]">
+      <h1 className="text-2xl mb-8 text-center">
         {title} ({year})
       </h1>
-      {documentToReactComponents(description, {
-        renderNode: {
-          [INLINES.HYPERLINK]: (node, children) => {
-            if (node.data.uri.includes("<iframe")) {
+      <div>
+        {documentToReactComponents(description, {
+          renderNode: {
+            [INLINES.HYPERLINK]: (node, children) => {
+              if (node.data.uri.includes("<iframe")) {
+                return (
+                  <span
+                    className="my-8 block aspect-video"
+                    dangerouslySetInnerHTML={{
+                      __html: node.data.uri,
+                    }}
+                  />
+                );
+              }
               return (
-                <span
-                  className="my-8 block"
-                  dangerouslySetInnerHTML={{
-                    __html: node.data.uri,
-                  }}
+                <a href={node.data.uri} target="__blank">
+                  {children}
+                </a>
+              );
+            },
+            [BLOCKS.PARAGRAPH]: (node, children) => {
+              if (
+                node.content.length === 1 &&
+                (node.content[0] as any).value === ""
+              ) {
+                return <br />;
+              }
+              return <p className="mb-4">{children}</p>;
+            },
+          },
+          preserveWhitespace: true,
+        })}
+      </div>
+      <div className="mt-16">
+        {files && files.length > 0 && (
+          <>
+            <div className="mb-2 font-bold">添付ファイル&gt;</div>
+            {files.map((file) => {
+              if (!file?.fields.file) return null;
+              return (
+                <FilePreview
+                  key={file.sys.id}
+                  url={file.fields.file.url}
+                  title={file.fields.title ?? "ファイル名不明"}
                 />
               );
-            }
-            return (
-              <a
-                href={node.data.uri}
-                target="__blank"
-                className="text-blue-500"
-              >
-                {children}
-              </a>
-            );
-          },
-          [BLOCKS.PARAGRAPH]: (node, children) => {
-            if (
-              node.content.length === 1 &&
-              (node.content[0] as any).value === ""
-            ) {
-              return <br />;
-            }
-            return <p>{children}</p>;
-          },
-        },
-        preserveWhitespace: true,
-      })}
-      <div>
-        {files &&
-          files.length > 0 &&
-          files.map((file) => {
-            if (!file?.fields.file) return null;
-            return (
-              <FilePreview
-                key={file.sys.id}
-                url={file.fields.file.url}
-                title={file.fields.title ?? "ファイル名不明"}
-              />
-            );
-          })}
+            })}
+          </>
+        )}
       </div>
     </main>
   );
