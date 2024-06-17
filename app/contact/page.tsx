717ef17f58e49237a04d547931a78e560fcdc248
yuichkun <yuichkun@gmail.com>
2024-06-18 01:12:01 +0900

    chore: Update contact page with bento link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,49 +5,10 @@ export default function Contact() {
   return (
     <main>
       <div>
-        <div className="mb-2">Social Media</div>
-        <ul>
-          <li>
-            X(日本語):{" "}
-            <a
-              href="https://twitter.com/yogo_yuichi"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              @yogo_yuichi
-            </a>
-          </li>
-          <li>
-            X(English):{" "}
-            <a
-              href="https://twitter.com/yuichi_yogo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              @yuichi_yogo
-            </a>
-          </li>
-          <li>
-            Instagram:{" "}
-            <a
-              href="https://www.instagram.com/yuichi_yogo/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              @yuichi_yogo
-            </a>
-          </li>
-          <li>
-            GitHub:{" "}
-            <a
-              href="https://github.com/yuichkun"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              @yuichkun
-            </a>
-          </li>
-        </ul>
+        <div className="mb-2">▶︎各種SNS等</div>
+        <a href="https://bento.me/yuichkun" target="_blank">
+          bento
+        </a>
       </div>
       <p className="mt-8">
         お仕事関係のご相談/お問い合わせは、
